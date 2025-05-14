7c192184f04b5888303640664d67f304b6cdeade
Davon Morris <davm2142@gmail.com>
2025-05-14 18:18:57 -0500

    Routes

--- server.js
+++ server.js
@@ -16,30 +16,27 @@ mongoose.connection.once('open', () => {
     console.log('Connected to MongoDB');
 });
 
-// Middleware to parse JSON
+// JSON parser
 app.use(express.json());
 
-// Routes
-app.use('/states', require('./routes/states'));
-
-// Catch-all 404
-app.all('*', (req, res) => {
-    if (req.accepts('html')) {
-        res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
-    } else if (req.accepts('json')) {
-        res.status(404).json({ error: '404 Not Found' });
-    } else {
-        res.status(404).type('txt').send('404 Not Found');
-    }
-});
-
-const path = require('path');
-
-// Serve root HTML page
+// 1) Root HTML page
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+// 2) API routes
+app.use('/states', require('./routes/states'));
+
+// 3) Catch-all 404
+app.all('*', (req, res) => {
+  if (req.accepts('html')) {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  } else if (req.accepts('json')) {
+    res.status(404).json({ error: '404 Not Found' });
+  } else {
+    res.status(404).type('txt').send('404 Not Found');
+  }
+});
 
 const PORT = process.env.PORT || 3500;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
